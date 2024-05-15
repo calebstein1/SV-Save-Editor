ec1c35fa699f0af1cc94378a7f69b51ec9fbd801
Caleb Stein <caleb@calebstein.net>
2024-05-14 22:56:26 -0700

    define SaveFile

---
+++ SVS-Season-Modifier.UI/Models/SaveFile.cs
@@ -0,0 +1,9 @@
+namespace SVS_Season_Modifier.UI.Models;
+
+internal class SaveFile
+{
+    internal string? FilePath { get; }
+    internal string? Name { get; }
+    internal string? CurSeason { get; }
+    internal string? CurSeasonByDay { get; }
+}
--- SVS-Season-Modifier.UI/ViewModels/MainWindowViewModel.cs
+++ SVS-Season-Modifier.UI/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,22 @@
-﻿namespace SVS_Season_Modifier.UI.ViewModels;
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
+using SVS_Season_Modifier.UI.Models;
 
-public class MainWindowViewModel : ViewModelBase
+namespace SVS_Season_Modifier.UI.ViewModels;
+
+internal class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    private ObservableCollection<SaveFile> _saveFiles = [];
+    internal ObservableCollection<SaveFile> SaveFiles
+    {
+        get => _saveFiles;
+        set => this.RaiseAndSetIfChanged(ref _saveFiles, value);
+    }
+
+    private string _status = "SVS Season Modifier v0.1.0-beta";
+    internal string Status
+    {
+        get => _status;
+        set => this.RaiseAndSetIfChanged(ref _status, value);
+    }
 }
